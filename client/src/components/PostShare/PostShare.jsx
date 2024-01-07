c9a0291c7840a8d9168cbbfbdab55a2131a7b3bc
Mohammed NIhal v K <mohammednihal691@gmail.com>
2024-01-07 15:59:57 +0530

    image input completed

--- client/src/components/PostShare/PostShare.jsx
+++ client/src/components/PostShare/PostShare.jsx
@@ -1,20 +1,21 @@
-import React,{useState,useRef} from 'react'
+import React, { useState, useRef } from 'react'
 import "./PostShare.css"
 import ProfileImage from "../../img/profileImg.jpg"
 import { UilScenery } from "@iconscout/react-unicons"
 import { UilPlayCircle } from "@iconscout/react-unicons"
 import { UilLocationPoint } from "@iconscout/react-unicons"
 import { UilSchedule } from "@iconscout/react-unicons"
+import { UilTimes } from "@iconscout/react-unicons"
 const PostShare = () => {
-    const [image,setImage] = useState(null)
+    const [image, setImage] = useState(null)
     const imageRef = useRef()
-    const onImageChange = (event)=>{
-    if(event.target.files && event.target.files[0]){
-        let img = event.target.files[0]
-        setImage({
-            image:URL.createObjectURL(img)
-        })
-    }
+    const onImageChange = (event) => {
+        if (event.target.files && event.target.files[0]) {
+            let img = event.target.files[0]
+            setImage({
+                image: URL.createObjectURL(img)
+            })
+        }
     }
     return (
         <div className='PostShare'>
@@ -23,33 +24,44 @@ const PostShare = () => {
                 <input type="text" placeholder="What's Happening" />
                 <div className="PostOption">
                     <div className="option"
-                    style={{color:"var(--photo)"}}
+                    onClick={()=>imageRef.current.click()}
+                        style={{ color: "var(--photo)" }}
                     >
                         <UilScenery />
                         Photo
                     </div >
-                    <div style={{color:"var(--vide0)"}}
-                     className="option">
+                    <div style={{ color: "var(--vide0)" }}
+                        className="option">
                         <UilPlayCircle />
                         Video
                     </div>
-                    <div style={{color:"var(--location)"}} 
-                    className="option">
+                    <div style={{ color: "var(--location)" }}
+                        className="option">
                         <UilLocationPoint />
                         Location
                     </div>
-                    <div style={{color:"var(--shedule)"}}
-                     className="option">
+                    <div style={{ color: "var(--shedule)" }}
+                        className="option">
                         <UilSchedule />
                         Shedule
                     </div>
                     <button className='button post-button'>
                         Share
                     </button>
-                    <div style={{display:"none"}}>
-                        <input type="file" name="MyImage" ref={imageRef} onChange={onImageChange}/>
+                    <div style={{ display: "none" }}>
+                        <input
+                            type="file"
+                            name="MyImage"
+                            ref={imageRef}
+                            onChange={onImageChange} />
                     </div>
                 </div>
+                {image && (
+                    <div className="previewImage">
+                        <UilTimes  onClick={()=>setImage(null)}/>
+                        <img src={image.image} alt="" />
+                    </div>
+                )}
             </div>
         </div>
     )
