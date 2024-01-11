fa06cba96603fc1bd15f893b8b81cbf7758fc5f0
Mohammed NIhal v K <mohammednihal691@gmail.com>
2024-01-11 22:46:40 +0530

    update

--- client/src/reducer/AuthReducer.js
+++ client/src/reducer/AuthReducer.js
@@ -18,8 +18,8 @@ const authReducer =
                 return { ...state, updateLoading: false, error: true }
             case 'FOLLOW_USER':
                 return { ...state, authData: { ...state.authData, user: { ...state.authData.user, following: [...state.authData.user.following, action.data] } } }
-                case 'UNFOLLOW_USER':
-                return { ...state, authData: { ...state.authData, user: { ...state.authData.user, following: [...state.authData.user.following.filter((personId)=>personId !== action.data) ] } }}
+            case 'UNFOLLOW_USER':
+                return { ...state, authData: { ...state.authData, user: { ...state.authData.user, following: [...state.authData.user.following.filter((personId) => personId !== action.data)] } } }
             case 'LOG_OUT':
                 localStorage.clear();
 
